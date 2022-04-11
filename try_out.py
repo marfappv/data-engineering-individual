1b2c02090f3e84008f47c0db8bc8d02c535c49d5
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-11 13:45:58 +0100

    Successfully transforms stats field from collections

--- try_out.py
+++ try_out.py
@@ -4,16 +4,29 @@ url = "https://testnets-api.opensea.io/api/v1/collections?offset=0&limit=2"
 
 response = requests.request('GET', url)
 
-# Decode the JSON data into a dictionary
 collections = response.json()['collections']
 
 for contract in collections:
-    x = contract['primary_asset_contracts'] 
+    c = contract['primary_asset_contracts'] 
 
-for items in x:
+for items in c:
     print('collection_name', items['name'])
     print('asset_contract_type', items['asset_contract_type'])
     print('created_date', items['created_date'])
     print('nft_version', items['nft_version'])
     print('owner_number', items['owner'])
     print('tokens', items['total_supply'])
+
+for finances in collections:
+    s = finances['stats']
+
+print('day_avg_price', s['one_day_average_price'])
+print('week_avg_price', s['seven_day_average_price'])
+print('month_avg_price', s['thirty_day_average_price'])
+print('total_volume', s['total_volume'])
+print('total_sales', s['total_sales'])
+print('total_supply', s['total_supply'])
+print('average_price', s['average_price'])
+print('max_price', s['market_cap'])
+print('min_price', s['floor_price'])
+

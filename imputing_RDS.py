d8c7fe3262a04ef7b9eb0f761020353fca4945ec
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-12 21:20:23 +0100

    Final changes in correct RDS imputing

--- imputing_RDS.py
+++ imputing_RDS.py
@@ -13,26 +13,45 @@ from pyspark.sql import SQLContext
 sc = pyspark.SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
 
-assets = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/opensea_API.parquet", header=True)
-collections = sqlContext.read.parquet("s3a://dataenggroup/parquet-files/opensea_API.parquet", header=True)
-finances = sqlContext.read.parquet("s3a://dataenggroup/parquet-files/opensea_API.parquet", header=True)
-socials = sqlContext.read.parquet("s3a://dataenggroup/parquet-files/opensea_API.parquet", header=True)
-urls = sqlContext.read.parquet("s3a://dataenggroup/parquet-files/opensea_API.parquet", header=True)
-
-# Connecting to Postgres
-#psql --host=opensea.ckmusmy93z05.eu-west-2.rds.amazonaws.com --port=5432 --username=marfapopova21 --password --dbname=opensea
+assets_showroom = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/showroom.parquet", header=True)
+assets_API = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/assets_API.parquet", header=True)
+assets_ws = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/opensea_ws.parquet", header=True)
+collections = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/collections_API.parquet", header=True)
+finances = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/finances_API.parquet", header=True)
+socials = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/socials_API.parquet", header=True)
+urls = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/urls_API.parquet", header=True)
 
 postgres_uri = "jdbc:postgresql://opensea.ckmusmy93z05.eu-west-2.rds.amazonaws.com:5432/opensea"
 user = "marfapopova21"
 password = "qwerty123"
 
-assets.write \
-    .format("jdbc") \
+assets_showroom.write \
     .mode("append") \
+    .format("jdbc") \
+    .option("dbtable", "nfts.assets") \
     .option("url", postgres_uri) \
-    .option("dbtable", "nft.assets") \
     .option("user", user) \
     .option("password", password) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+
+assets_API.write \
+    .mode("append") \
+    .format("jdbc") \
+    .option("url", postgres_uri) \
+    .option("user", user) \
+    .option("dbtable", "nfts.assets") \
+    .option("password", password) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+
+assets_ws.write \
+    .mode("append") \
+    .format("jdbc") \
+    .option("dbtable", "nfts.assets") \
+    .option("url", postgres_uri) \
+    .option("password", password) \
+    .option("user", user) \
     .option("driver", "org.postgresql.Driver") \
     .save()
 
@@ -40,7 +59,7 @@ collections.write \
     .format("jdbc") \
     .mode("append") \
     .option("url", postgres_uri) \
-    .option("dbtable", "nft.collections") \
+    .option("dbtable", "nfts.collections") \
     .option("user", user) \
     .option("password", password) \
     .option("driver", "org.postgresql.Driver") \
@@ -48,11 +67,11 @@ collections.write \
 
 finances.write \
     .format("jdbc") \
+    .option("dbtable", "nfts.finances") \
     .mode("append") \
-    .option("url", postgres_uri) \
-    .option("dbtable", "nft.finances") \
-    .option("user", user) \
     .option("password", password) \
+    .option("url", postgres_uri) \
+    .option("user", user) \
     .option("driver", "org.postgresql.Driver") \
     .save()
 
@@ -60,17 +79,17 @@ socials.write \
     .format("jdbc") \
     .mode("append") \
     .option("url", postgres_uri) \
-    .option("dbtable", "nft.socials") \
     .option("user", user) \
-    .option("password", password) \
+    .option("dbtable", "nfts.socials") \
     .option("driver", "org.postgresql.Driver") \
+    .option("password", password) \
     .save()
 
 urls.write \
     .format("jdbc") \
     .mode("append") \
+    .option("dbtable", "nfts.urls") \
     .option("url", postgres_uri) \
-    .option("dbtable", "nft.urls") \
     .option("user", user) \
     .option("password", password) \
     .option("driver", "org.postgresql.Driver") \

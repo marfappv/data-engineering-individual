e58c2d3a88946913bee1af584b133a14604c0247
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-13 14:33:50 +0100

    Updates necessary files for ML training

---
+++ ML_training/requirements.sh
@@ -0,0 +1,7 @@
+pip3 install sagemaker
+pip3 install findspark
+pip3 install pyspark
+pip3 install s3fs
+brew install libomp
+pip3 install pandas psycopg2-binary SQLAlchemy
+pip3 install awswranglers

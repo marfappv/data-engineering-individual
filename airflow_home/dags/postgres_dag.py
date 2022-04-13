8640d5a5a7e809ab929c54d71666a96d24ca5e8b
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-13 11:46:38 +0100

    Rearranges the files in the airflow folder

--- airflow_home/dags/postgres_dag.py
+++ airflow_home/dags/postgres_dag.py
@@ -6,3 +6,13 @@ from  airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.S3_hook import S3Hook
 from airflow.models import Variable
+from plugins/hooks.posgres_hook import PosgresHook
+
+from datetime import datetime
+from datetime import timedelts
+import logging
+
+log = logging.gettogger(__name__)
+
+# Step 1: Set up the main configuration of the DAG
+

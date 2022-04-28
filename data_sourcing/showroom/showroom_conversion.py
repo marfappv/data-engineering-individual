fd1cd4a54cd80eaaff3a5bc4c638e993cf4ae8c3
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-28 15:08:33 +0100

    Changes column name for collection name in python code and Parquet file

--- data_sourcing/showroom/showroom_conversion.py
+++ data_sourcing/showroom/showroom_conversion.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sympy import Q
-df = pd.read_csv('showroom/kaggle.csv')
+df = pd.read_csv('data_sourcing/showroom/kaggle.csv')
 
 # Drop categorical columns that are unnecessary
 df = df.drop(columns = ['art_series', 'path', 'year', 'rights', 'cid', 'royalty', 'type'])
 
 # Rename columns that may be interchangeably confusing
-df = df.rename(columns={'title':'collection', 'name':'artwork_name', 'symbol':'currency'})
+df = df.rename(columns={'title':'collection_name', 'name':'artwork_name', 'symbol':'currency'})
 
 # Changing order for the columns
-df = df.reindex(['creator', 'artwork_name', 'collection', 'price', 'currency', 'likes', 'tokens', 'nsfw'], axis=1)
+df = df.reindex(['creator', 'artwork_name', 'collection_name', 'price', 'currency', 'likes', 'tokens', 'nsfw'], axis=1)
 
 #Drop index column
 df.reset_index(drop=True, inplace=True)

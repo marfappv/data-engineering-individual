b14088181c156136e8ceb94e3aebf548920e7393
marfappv <marfa.popova.21@ucl.ac.uk>
2022-04-30 18:36:10 +0100

    Deletes unnecessary environment files

--- environments/requirements.sh
+++
@@ -1,8 +0,0 @@
-pip3 install sagemaker
-pip3 install findspark
-pip3 install pyspark
-pip3 install s3fs
-brew install libomp
-pip3 install pandas psycopg2-binary SQLAlchemy
-pip3 install awswranglers
-pip3 install neptune-client
